437630d7c8ca890fb0928502c33dd2d31174fe4d
cubnoble <cub.noble@yahoo.fr>
2022-04-29 12:49:34 -0500

    Setting up the table. Allowing cards to be pulled from hand.

--- Assets/Script/CardPlacement.cs
+++ Assets/Script/CardPlacement.cs
@@ -5,13 +5,36 @@ using UnityEngine;
 public class CardPlacement : MonoBehaviour
 {
     public Vector3 offset = new Vector3(0, 30, 0);
+    public Vector3 pullFromHand = new Vector3(0, -175f, 0);
+
+    bool hasEntered;
+    bool hasBeenPulled;
     public void CardHoverEnter()
     {
         this.gameObject.transform.localPosition += offset;
+        hasEntered = true;
     }
 
     public void CardHoverExit()
     {
         this.gameObject.transform.localPosition -= offset;
+        hasEntered = false;
+    }
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && hasEntered && !hasBeenPulled)
+        {
+            this.gameObject.transform.localPosition -= pullFromHand;
+            GameObject.Find("PlayerHand").transform.localPosition += pullFromHand;
+            hasBeenPulled = true;
+        }
+
+        if (Input.GetMouseButtonDown(1) && hasEntered && hasBeenPulled)
+        {
+            this.gameObject.transform.localPosition += pullFromHand;
+            GameObject.Find("PlayerHand").transform.localPosition -= pullFromHand;
+            hasBeenPulled = false;
+        }
     }
 }
