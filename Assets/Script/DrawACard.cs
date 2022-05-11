9b3a317556dee089308d7891608432e0e1c3a4f4
cubnoble <cub.noble@yahoo.fr>
2022-05-10 21:40:29 -0500

    Player draws initial hand of 5 cards

--- Assets/Script/DrawACard.cs
+++ Assets/Script/DrawACard.cs
@@ -8,16 +8,51 @@ public class DrawACard : MonoBehaviour
     public GameObject[] cards;
     public Transform playerHand;
 
+    public int initialCardsToDraw = 5;
+    int cardCount;
+    //Player Hand - remove this later to a more accessible place
+    public List<Card> monsterCards = new List<Card>();
+    public List<SpellCard> spellCards = new List<SpellCard>();
+    List<MonoBehaviour> list = new List<MonoBehaviour>();
+
+    private void Start()
+    {
+        //draw initial hand
+        StartCoroutine(WaitToDrawCard());
+    }
+
     private void OnMouseOver()
     {
-        //This draws a new card from the player's deck
-            //Right now, it is pulling the same gameobject. Eventually, this will be a dynamic list
         if (Input.GetMouseButtonDown(0))
         {
-            GameObject newCard = Instantiate(cards[0], playerHand.transform.position, Quaternion.identity);
-            newCard.transform.parent = playerHand;
-            newCard.transform.localScale = new Vector3(1, 1, 1);
-            newCard.transform.localRotation = Quaternion.identity;
+            DrawCard();
+        }
+    }
+
+    void DrawCard()
+    {
+        //list of monster cards
+        //list of spell cards
+        //run a deck random number counter
+        //if random number < total spell cards, pull a spell card, else, pull a monster card
+        //resize the given list
+        GameObject newCard = Instantiate(cards[0], playerHand.transform.position, Quaternion.identity);
+        newCard.transform.parent = playerHand;
+        newCard.transform.localScale = new Vector3(1, 1, 1);
+        newCard.transform.localRotation = Quaternion.identity;
+
+        cardCount++;
+    }
+
+    IEnumerator WaitToDrawCard()
+    {
+        //While initial cards in hand is less than initialCardsToDraw, draw another card but wait 0.25f
+        while (initialCardsToDraw > 0)
+        {
+            //Wait .25f seconds
+            yield return new WaitForSeconds(0.25f);
+            DrawCard();
+            initialCardsToDraw--;
         }
     }
 }
