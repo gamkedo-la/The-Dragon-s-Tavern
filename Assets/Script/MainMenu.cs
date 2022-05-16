19e9d93bb7742c2b017995b4a699807978f97715
Simon Donohoe <simon.donohoe@gmail.com>
2022-05-16 17:22:49 +0100

    Added a basic main menu

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void ExitButton() // exit the game from the main menu
+    {
+        Application.Quit();
+        Debug.Log("Exiting the game");
+    }
+    public void StartGame() // start the game by pressing the play button on the main menu
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
