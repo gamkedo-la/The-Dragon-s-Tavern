f93357c14e1d998e5f2c79507bf1e0946944d990
cubnoble <cub.noble@yahoo.fr>
2022-07-20 20:53:56 -0500

    Removing graveyard. Fixing '0 cards to draw' bug

--- Assets/Script/DrawACard.cs
+++ Assets/Script/DrawACard.cs
@@ -40,62 +40,68 @@ public class DrawACard : MonoBehaviour
     public void DrawCard()
     {
         //List of Monster Cards and Spell cards are public right now. May want to hide that eventually.
-
-        //run a deck random number counter
-        int randomCardFromDeck = Random.Range(0, totalCardsInDeck);
-        //pulling a monster card
-        if (randomCardFromDeck >= spellCards.Count)
+        if (totalCardsInDeck <= 0)
         {
-            //take list of cards (monsters+spells), subtract the spell cards, then pull a monster card
-            int monsterCardToMake = randomCardFromDeck - spellCards.Count;
-            cardNameToSave = monsterCards[monsterCardToMake].ToString();
-            newCard = Instantiate(cards[1], playerHand.transform.position, Quaternion.identity) as GameObject;
-
-            //Making the card name match the card that is drawn
-            cardNameToSave = cardNameToSave.Replace(" (Card)", "");
-
-            Card tempCard = Resources.Load<Card>("ScriptableObject/Monsters/" + cardNameToSave) as Card;
-
-            newCard.GetComponentInChildren<CardDisplay>().card = tempCard;
-
-            newCard.GetComponentInChildren<CardDisplay>().ReadyToInit();
-
-            //Remove Monster Card from List
-            monsterCards.Remove(monsterCards[monsterCardToMake]);
+            print("Player has no more cards to draw. The game is over.");
         }
-
-        //pulling a spell card
         else
         {
-            cardNameToSave = spellCards[randomCardFromDeck].ToString();
-            newCard = Instantiate(cards[0], playerHand.transform.position, Quaternion.identity);
+            //run a deck random number counter
+            int randomCardFromDeck = Random.Range(0, totalCardsInDeck);
+            //pulling a monster card
+            if (randomCardFromDeck >= spellCards.Count)
+            {
+                //take list of cards (monsters+spells), subtract the spell cards, then pull a monster card
+                int monsterCardToMake = randomCardFromDeck - spellCards.Count;
+                cardNameToSave = monsterCards[monsterCardToMake].ToString();
+                newCard = Instantiate(cards[1], playerHand.transform.position, Quaternion.identity) as GameObject;
 
-            //Making the card name match the card that is drawn
-            cardNameToSave = cardNameToSave.Replace(" (SpellCard)", "");
+                //Making the card name match the card that is drawn
+                cardNameToSave = cardNameToSave.Replace(" (Card)", "");
 
-            SpellCard tempCard = Resources.Load<SpellCard>("ScriptableObject/Spell/" + cardNameToSave) as SpellCard;
+                Card tempCard = Resources.Load<Card>("ScriptableObject/Monsters/" + cardNameToSave) as Card;
 
-            newCard.GetComponentInChildren<CardDisplay>().spellCard = tempCard;
+                newCard.GetComponentInChildren<CardDisplay>().card = tempCard;
 
-            newCard.GetComponentInChildren<CardDisplay>().ReadyToInit();
+                newCard.GetComponentInChildren<CardDisplay>().ReadyToInit();
 
-            //Remove Spell card from list
-            spellCards.Remove(spellCards[randomCardFromDeck]);
-        }
+                //Remove Monster Card from List
+                monsterCards.Remove(monsterCards[monsterCardToMake]);
+            }
 
-        //Create the card
+            //pulling a spell card
+            else
+            {
+                cardNameToSave = spellCards[randomCardFromDeck].ToString();
+                newCard = Instantiate(cards[0], playerHand.transform.position, Quaternion.identity);
 
-        newCard.transform.parent = playerHand;
-        newCard.transform.localScale = new Vector3(1, 1, 1);
-        newCard.transform.localRotation = Quaternion.identity;
+                //Making the card name match the card that is drawn
+                cardNameToSave = cardNameToSave.Replace(" (SpellCard)", "");
 
-         totalCardsInDeck--;
-        //Update UI of cards in player's deck
-         deckCountRemainingUI.text = totalCardsInDeck.ToString();
-        //Remove cards that are on the table if less than 10 remain
-        if (totalCardsInDeck < 10)
-        {
-            physicalCardsOnTable[totalCardsInDeck].SetActive(false);
+                SpellCard tempCard = Resources.Load<SpellCard>("ScriptableObject/Spell/" + cardNameToSave) as SpellCard;
+
+                newCard.GetComponentInChildren<CardDisplay>().spellCard = tempCard;
+
+                newCard.GetComponentInChildren<CardDisplay>().ReadyToInit();
+
+                //Remove Spell card from list
+                spellCards.Remove(spellCards[randomCardFromDeck]);
+            }
+
+            //Create the card
+
+            newCard.transform.parent = playerHand;
+            newCard.transform.localScale = new Vector3(1, 1, 1);
+            newCard.transform.localRotation = Quaternion.identity;
+
+            totalCardsInDeck--;
+            //Update UI of cards in player's deck
+            deckCountRemainingUI.text = totalCardsInDeck.ToString();
+            //Remove cards that are on the table if less than 10 remain
+            if (totalCardsInDeck < 10)
+            {
+                physicalCardsOnTable[totalCardsInDeck].SetActive(false);
+            }
         }
     }
 
