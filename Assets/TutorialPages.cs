a65ca6769dd692315e8e0c375aae19ef96bbd6e8
cubnoble <cub.noble@yahoo.fr>
2022-08-13 21:02:49 -0500

    Tutorial book created

---
+++ Assets/TutorialPages.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialPages : MonoBehaviour
+{
+    public int pageNumber = 0;
+    public GameObject[] pages;
+
+    public Button left, right;
+
+    public Animator pageTurning;
+
+    private void Update()
+    {
+        if (pageNumber <= 0)
+        {
+            pageNumber = 0;
+            left.interactable = false;
+        }        
+        else {
+            left.interactable = true;
+        }
+
+        if (pageNumber >= pages.Length - 1)
+        {
+            right.interactable = false;
+        }
+        else
+        {
+            right.interactable = true;
+        }
+    }
+
+    public void TurnRight()
+    {
+        pageTurning.SetTrigger("RotateLeft");
+        pages[pageNumber].SetActive(false);
+        pageNumber--;
+        StartCoroutine(DelayForPageTurn());
+    }
+
+    public void TurnLeft()
+    {
+        pageTurning.SetTrigger("RotateRight");
+        pages[pageNumber].SetActive(false);
+        pageNumber++;
+        StartCoroutine(DelayForPageTurn());
+    }
+
+    IEnumerator DelayForPageTurn()
+    {
+        yield return new WaitForSeconds(1.25f);
+        pages[pageNumber].SetActive(true);
+    }
+}
